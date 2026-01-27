9495ac67a87f3d61459b80f83254bf1484afa151
sachx1 <sachx1281@gmail.com>
2026-01-26 22:49:34 -0500

    added new page

---
+++ app/homeAfter/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function homeAfter() {
+  return (
+    <div>
+      <h1>First page</h1>
+      <Link href="/">Back to home page</Link>
+    </div>
+  );
+}
