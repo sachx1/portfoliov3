097bd20eaec90201c45f5ca3fc56826cfe88a12a
sachx1 <sachx1281@gmail.com>
2026-01-29 22:52:08 -0500

    added typist animation library to the project

---
+++ app/introSection/page.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Typewriter } from 'nextjs-simple-typewriter'
+
+export default function IntroSection() {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import LoadingScreen from "./loadingScreen/page";
+import { Typewriter } from 'nextjs-simple-typewriter'
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -13,12 +14,30 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, []);
 
+  const handleType = (count: number) => {
+    console.log(count)
+  }
+
+  const handleDone = () => {
+    console.log("Done after 5 loops!");
+  }
+
   if (loading) return <LoadingScreen />;
 
   return (
     <main className="flex items-center justify-center h-screen bg-black text-white">
       <h1 className="text-5xl font-mono neon-welcome">
-        Welcome to Night City
+        <Typewriter
+            words={[`Welcome to Sachin Khargie's Page`, `Take a look`, `At their experience`]}
+            loop={1}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            onLoopDone={handleDone}
+            onType={handleType}
+          />
       </h1>
     </main>
   );
