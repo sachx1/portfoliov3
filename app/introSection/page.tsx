93b1def0c18c6debd15be689f055c702c961402d
sachx1 <sachx1281@gmail.com>
2026-01-30 22:03:26 -0500

    playing around with typing animation

--- app/introSection/page.tsx
+++ app/introSection/page.tsx
@@ -2,10 +2,57 @@ import { useEffect, useState } from "react";
 import { Typewriter } from 'nextjs-simple-typewriter'
 
 export default function IntroSection() {
+    const [step, setStep] = useState(0);
+
+    const handleType = (count: number) => {
+        const fullWord = "Sachin Khargie";
+        let isStep = false;
+
+        // When count reaches the full length, typing is done
+        if (count === fullWord.length) {
+            setStep(1);
+            console.log('1');
+        }
+
+        if (step === 1){
+            isStep = true;
+        }
+
+        return isStep;
+    };
+
+    const finishedFirstWord = handleType
 
     return (
         <div>
+            <h1 className="text-5xl font-mono neon-welcome">
+                <Typewriter
+                    words={[`Sachin Khargie`]}
+                    loop={1}
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                    onType={handleType}
+                />
+            </h1>
             
+            {handleType &&
+
+                <h1 className="text-5xl font-mono neon-welcome">
+                    <Typewriter
+                        words={['Full‑Stack Developer']}
+                        loop={1}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                        onType={handleType}
+                    />
+                </h1>
+            }
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import LoadingScreen from "./loadingScreen/page";
 import { Typewriter } from 'nextjs-simple-typewriter'
+import IntroSection from "./introSection/page";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -14,31 +15,11 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, []);
 
-  const handleType = (count: number) => {
-    console.log(count)
-  }
-
-  const handleDone = () => {
-    console.log("Done after 5 loops!");
-  }
-
   if (loading) return <LoadingScreen />;
 
   return (
     <main className="flex items-center justify-center h-screen bg-black text-white">
-      <h1 className="text-5xl font-mono neon-welcome">
-        <Typewriter
-            words={[`Welcome to Sachin Khargie's Page`, `Take a look`, `At their experience`]}
-            loop={1}
-            cursor
-            cursorStyle='_'
-            typeSpeed={70}
-            deleteSpeed={50}
-            delaySpeed={1000}
-            onLoopDone={handleDone}
-            onType={handleType}
-          />
-      </h1>
+      <IntroSection />
     </main>
   );
 }
